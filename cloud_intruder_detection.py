550c66757d9a3a2ab41bb7b6d9b7e072e583fc15
Hpone-Myat-Min <hponemyatmin@my.jcu.edu.au>
2025-09-05 18:38:56 +0100

    added main function

--- cloud_intruder_detection.py
+++ cloud_intruder_detection.py
@@ -4,6 +4,7 @@ from picamera2 import Picamera2, Preview
 from datetime import datetime
 import time
 import requests
+import serial
 
 EC2_API_URL = "http://56.228.35.90:5000/detect"
 s3 = boto3.client('s3')
@@ -77,3 +78,14 @@ def start_monitoring():
 
     # image_queue.append(image_paths)
     is_monitoring = False
+
+if __name__ == "__main__":
+    serial_port = serial.Serial('/dev/rfcomm0', baudrate=9600, timeout=1)
+
+    print("Starting bluetooth")
+
+    while True:
+        motion_status = serial_port.readline().decode('utf-8').strip()
+        if motion_status == "MOTION_DETECTED" and not is_monitoring:
+            print("PIR detects motion and now camera will be open")
+            start_monitoring()
